dfbcc618fa05fb71c5d7cd21f7545f1579641704
Mohammednaveed <naveedmohammed687@gmail.com>
2023-10-11 10:25:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import VehicleType from "./components/Vehicletype/VehicleType";
+import BusPage from "./components/Vehicletype/bus";
+import Othervehicle from "./components/Vehicletype/othervehicle"
+import { auth } from "./firebase";
+import "./App.css";
+import "./components/Vehicletype/busstyle.css"
+function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+      <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/VehicleType" element={<VehicleType />} />
+          <Route path="/Bus" element={<BusPage />}/>
+          <Route path="/Othervehicle" element={<Othervehicle />} />
+          <Route path="/" element={<Home name={userName} />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
